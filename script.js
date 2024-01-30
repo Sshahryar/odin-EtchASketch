788567f99a80c3c0a7ed266149f5d599bb8713db
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-01-29 21:00:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,18 @@ document.addEventListener('DOMContentLoaded', function() {
     square.classList.add('square');
     container.appendChild(square);
   }
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => {
+    square.addEventListener('mouseenter', function() {
+      square.style.backgroundColor = getRandomColor();
+    });
+  });
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 });
